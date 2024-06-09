a42f0d3e5f9eab0744fa0262bb92a8ab61c96293
arparvej1 <parvej8pro@gmail.com>
2024-06-09 20:17:17 +0600

    Filter By Role

--- src/pages/DashBoard/AdminDashboard/ManageUsers/ManageUsers copy.jsx
+++ src/pages/DashBoard/AdminDashboard/ManageUsers/ManageUsers copy.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import useAxiosSecure from '../../../../hooks/useAxiosSecure';
 import useAuth from '../../../../hooks/useAuth';
 import { Helmet } from 'react-helmet-async';
@@ -8,44 +8,40 @@ import { ToastContainer, toast } from 'react-toastify';
 const ManageUsers = () => {
   const axiosSecure = useAxiosSecure();
   const { user } = useAuth();
-  const [loadAllUsers, setLoadAllUsers] = useState([]);
   const [allUsers, setAllUsers] = useState([]);
-
-  const callLoadAllUsers = async () => {
-    try {
-      const res = await axiosSecure.get(`/allUsers/${user.email}`);
-      setAllUsers(res.data);
-      setLoadAllUsers(res.data);
-      // console.log(res.data);
-    } catch (error) {
-      console.error('Error fetching applied scholarships:', error);
-    }
-  };
+  const [sortOption, setSortOption] = useState('name');
+  const [filterOption, setFilterOption] = useState('all');
 
   useEffect(() => {
-    callLoadAllUsers();
-  }, []);
+    const loadAllUsers = async () => {
+      try {
+        const res = await axiosSecure.get(`/allUsers/${user.email}`);
+        setAllUsers(res.data);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+    loadAllUsers();
+  }, [axiosSecure, user.email]);
 
-  const handleRoleChange = (selectUser, newRole) => {
+  const handleRoleChange = (e, selectUser) => {
     if (selectUser.email === user.email) {
-      return toast.error('You cannot change your role!')
+      return toast.error('You cannot change your role!');
     }
+    const newRole = e.target.value;
     const updatedUsers = allUsers.map(user => user._id === selectUser._id ? { ...user, role: newRole } : user);
     setAllUsers(updatedUsers);
   };
 
   const handleMakeChange = (selectUser) => {
     if (selectUser.email === user.email) {
-      return toast.error('You cannot change your role!')
+      return toast.error('You cannot change your role!');
     }
-    // console.log(selectUser);
     const new_role = {
       role: selectUser.role,
-    }
-    // --------- send server start -----
+    };
     axiosSecure.patch(`/updateUser/${selectUser._id}`, new_role)
       .then(function (response) {
-        // console.log(response.data);
         if (response.data.modifiedCount > 0) {
           Swal.fire({
             position: "top-end",
@@ -54,19 +50,16 @@ const ManageUsers = () => {
             showConfirmButton: false,
             timer: 1500
           });
-          const updatedUsers = loadAllUsers.map(user => user._id === selectUser._id ? { ...user, role: selectUser.role } : user);
-          setLoadAllUsers(updatedUsers);
         }
       })
       .catch(function (error) {
         console.log(error);
       });
-    // --------- send server end -----
   };
 
   const handleDeleteUser = (selectUser) => {
     if (selectUser.email === user.email) {
-      return toast.error('You cannot delete it yourself!')
+      return toast.error('You cannot delete it yourself!');
     }
     Swal.fire({
       title: "Are you sure?",
@@ -78,10 +71,8 @@ const ManageUsers = () => {
       confirmButtonText: "Yes, delete it!"
     }).then((result) => {
       if (result.isConfirmed) {
-        // --------- send server start -----
         axiosSecure.delete(`/userDelete/${selectUser.email}`)
           .then(function (response) {
-            console.log(response.data);
             if (response.data.deletedCount > 0) {
               const updatedUsers = allUsers.filter(user => user._id !== selectUser._id);
               setAllUsers(updatedUsers);
@@ -97,39 +88,33 @@ const ManageUsers = () => {
           .catch(function (error) {
             console.log(error);
           });
-        // --------- send server end -----
       }
     });
   };
-  // ---------- filter start ----------- 
+
   const handleSortChange = (e) => {
-    const selectedValue = e.target.value;
-    let sortedUsers = [...allUsers]; // Create a copy of allUsers array
-
-    // Sort the array based on the selected value
-    sortedUsers.sort((a, b) => {
-      if (a[selectedValue] < b[selectedValue]) return -1;
-      if (a[selectedValue] > b[selectedValue]) return 1;
-      return 0;
-    });
-
-    // Update the state with the sorted array
-    setAllUsers(sortedUsers);
+    setSortOption(e.target.value);
   };
 
   const handleFilterChange = (e) => {
-    const selectedValue = e.target.value;
-    let sortedUsers = [...allUsers]; // Create a copy of allUsers array
-
-    if (selectedValue !== 'all') {
-      sortedUsers = loadAllUsers.filter(user => user.role === selectedValue);
-      setAllUsers(sortedUsers);
-    } else {
-      setAllUsers(loadAllUsers);
-    }
-    console.log(selectedValue);
+    setFilterOption(e.target.value);
   };
-  // ---------- filter end ----------- 
+
+  // Apply sorting and filtering
+  let sortedAndFilteredUsers = [...allUsers];
+  sortedAndFilteredUsers.sort((a, b) => {
+    if (sortOption === 'name') {
+      return a.name.localeCompare(b.name);
+    } else if (sortOption === 'email') {
+      return a.email.localeCompare(b.email);
+    } else if (sortOption === 'role') {
+      return a.role.localeCompare(b.role);
+    }
+  });
+
+  if (filterOption !== 'all') {
+    sortedAndFilteredUsers = sortedAndFilteredUsers.filter(user => user.role === filterOption);
+  }
 
   return (
     <div>
@@ -137,19 +122,19 @@ const ManageUsers = () => {
         <title> Manage Users | ScholarPoint </title>
       </Helmet>
       <h3 className="bg-base-300 w-full p-5 md:p-8 text-2xl md:text-5xl font-bold text-center rounded-3xl my-5">Manage Users</h3>
-      {/* ---------- filter start ----------- */}
+      
       <div className="flex justify-end space-x-4 p-4">
-        <div className='flex gap-2'>
+        <div>
           <label htmlFor="sort">Sort By:</label>
-          <select className='border-2' id="sort" onChange={handleSortChange}>
+          <select id="sort" value={sortOption} onChange={handleSortChange}>
             <option value="name">Name</option>
             <option value="email">Email</option>
             <option value="role">Role</option>
           </select>
         </div>
-        <div className='flex gap-2'>
+        <div>
           <label htmlFor="filter">Filter By Role:</label>
-          <select className='border-2' id="filter" onChange={handleFilterChange}>
+          <select id="filter" value={filterOption} onChange={handleFilterChange}>
             <option value="all">All</option>
             <option value="user">User</option>
             <option value="agent">Moderator</option>
@@ -157,10 +142,9 @@ const ManageUsers = () => {
           </select>
         </div>
       </div>
-      {/* ---------- filter end ----------- */}
+
       <div className="overflow-x-auto">
         <table className="table">
-          {/* head */}
           <thead>
             <tr>
               <th>User Name</th>
@@ -171,15 +155,14 @@ const ManageUsers = () => {
             </tr>
           </thead>
           <tbody>
-            {/* Replace this with your mapping logic */}
-            {allUsers.map(thisUser => (
+            {sortedAndFilteredUsers.map(thisUser => (
               <tr key={thisUser._id}>
                 <td>{thisUser.name}</td>
                 <td>{thisUser.email}</td>
                 <td>
                   <select
                     value={thisUser.role}
-                    onChange={(e) => handleRoleChange(thisUser, e.target.value)}
+                    onChange={(e) => handleRoleChange(e, thisUser)}
                   >
                     <option value="user">User</option>
                     <option value="agent">Moderator</option>
